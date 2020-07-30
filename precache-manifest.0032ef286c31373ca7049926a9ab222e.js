5a3b9adbba6f8908933827054d119fb3b5affe31
jasperhuangg <jasperhu@usc.edu>
2020-07-30 10:55:29 +0800

    Updates

---
+++ precache-manifest.0032ef286c31373ca7049926a9ab222e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25facac90d45e0506f969a7f5a09858b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dd01b84b94c4bd796010",
+    "url": "/portfolio/static/css/main.a6419e50.chunk.css"
+  },
+  {
+    "revision": "e547b8d90ae9c1beefe2",
+    "url": "/portfolio/static/js/2.21f18ea8.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.21f18ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd01b84b94c4bd796010",
+    "url": "/portfolio/static/js/main.32f687f6.chunk.js"
+  },
+  {
+    "revision": "9d244302c4d41b89a93a",
+    "url": "/portfolio/static/js/runtime-main.2d21c647.js"
+  },
+  {
+    "revision": "48109b3ac7031d3b7df2574c8f010a4e",
+    "url": "/portfolio/static/media/Jasper_Huang_Resume.48109b3a.pdf"
+  },
+  {
+    "revision": "0ed224a5bd38a37b01d089f01b178804",
+    "url": "/portfolio/static/media/preview.0ed224a5.gif"
+  },
+  {
+    "revision": "3dcfb849f1b8071e6ec3b7bae9660fc7",
+    "url": "/portfolio/static/media/preview.3dcfb849.gif"
+  },
+  {
+    "revision": "4f6aae3429352cb278415f3042944a8f",
+    "url": "/portfolio/static/media/preview.4f6aae34.png"
+  },
+  {
+    "revision": "7d9d7588e881cb295dcf50a6c70c76bd",
+    "url": "/portfolio/static/media/preview.7d9d7588.gif"
+  },
+  {
+    "revision": "8f6482b0b5f80943cf48125e9be0f18b",
+    "url": "/portfolio/static/media/preview.8f6482b0.png"
+  },
+  {
+    "revision": "e06ca9183c874fca9464cccfdad15bb1",
+    "url": "/portfolio/static/media/preview.e06ca918.gif"
+  },
+  {
+    "revision": "23ae29cff15becd829501874372fa850",
+    "url": "/portfolio/static/media/sf-pro-text-bold.23ae29cf.ttf"
+  },
+  {
+    "revision": "f113e39976799c1611cd3aebb3418ecb",
+    "url": "/portfolio/static/media/sf-pro-text-heavy.f113e399.ttf"
+  },
+  {
+    "revision": "ed2b077e473f532241e0daa43f62f2b5",
+    "url": "/portfolio/static/media/sf-pro-text-light.ed2b077e.ttf"
+  },
+  {
+    "revision": "8b61dea99518b51d5511dc3963a5537c",
+    "url": "/portfolio/static/media/sf-pro-text-medium.8b61dea9.ttf"
+  },
+  {
+    "revision": "12d8580f658f54bd0a4c4fc5a5983a94",
+    "url": "/portfolio/static/media/sf-pro-text-regular.12d8580f.ttf"
+  },
+  {
+    "revision": "77690a3c3c360557395489fa2e9ce27a",
+    "url": "/portfolio/static/media/sf-pro-text-semibold.77690a3c.ttf"
+  }
+]);
